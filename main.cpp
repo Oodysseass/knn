08ec384569cfca1b669854fbd70b3d4ade771375
Odysseas Sofikitis <o.sofikitis@gmail.com>
2022-12-25 13:15:25 +0200

    Fixes includes, functions declarations, removes namespace

--- main.cpp
+++ main.cpp
@@ -1,70 +1,66 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <mpi.h>
 
-using namespace std;
+#include <algorithm>
+#include <numeric>
+#include <cmath>
+#include <gsl/gsl_cblas.h>
+
 
 typedef struct {
-    int **nidx;
-    double **ndist;
+    int *nidx;
+    double *ndist;
     int m;
     int k;
 } Knnresult;
 
-Knnresult* kNN(double **query, double **corpus, int m, int n, int d, int k);
+Knnresult* kNN(double *query, double *corpus, int m, int n, int d, int k);
 
-double** Hammard(double **A, double **B, int rows, int cols);
-
-double* RowSum(double **A, int rows, int cols);
-
-double** Transpose(double **A, int rows, int cols);
-
-double** MatrixMultiplication(double **A, double **B, int rowsA, int colsA, int rowsB, int colsB);
-
+double* calculateDistance(double *query, double *corpus, int m, int n, int d);
 
 
 int main(int argc, char* argv[])
 {
     if (argc < 3)
     {
-        cout << "Usage: ./main <filename> <numberofneighbors>" << endl;
+        std::cout << "Usage: ./main <filename> <numberofneighbors>" << std::endl;
         return 1;
     }
 
-    ifstream dataFile(argv[1]);
+    std::ifstream dataFile(argv[1]);
     if (!dataFile)
-        cout << "Error in opening of the file" << endl;
+        std::cout << "Error in opening of the file" << std::endl;
 
     int numNeighbors = atoi(argv[2]);
-    int numOfPoints;
+    int numOfCorpus;
     int dimensions;
-    dataFile >> numOfPoints >> dimensions;
+    dataFile >> numOfCorpus >> dimensions;
     
-    double** corpusPoints =  new double*[numOfPoints];
-    for (int i = 0; i < numOfPoints; i++)
-        corpusPoints[i] = new double[dimensions];
+    double* corpusPoints = new double[numOfCorpus * dimensions];
 
-    for (int i = 0; i < numOfPoints; i++)
-        for (int j = 0; j < dimensions; j++)
-            dataFile >> corpusPoints[i][j];
+    for (int i = 0; i < numOfCorpus * dimensions; i++)
+        dataFile >> corpusPoints[i];
 
-    Knnresult* knnresult = kNN(corpusPoints, corpusPoints, numOfPoints, numOfPoints, dimensions, numNeighbors);
+    // this is just temporary
+    int numOfQuery = 2;
+    double* queryPoints = new double[numOfQuery * dimensions];
+
+    for (int i = 0; i < numOfQuery * dimensions; i++)
+        queryPoints[i] = corpusPoints[i];
+    //
+
+    Knnresult* knnresult = kNN(queryPoints, corpusPoints, numOfQuery, numOfCorpus, dimensions, numNeighbors);
 
     return 0;
 }
 
-Knnresult* kNN(double **query, double **corpus, int m, int n, int d, int k)
+
+Knnresult* kNN(double *query, double *corpus, int m, int n, int d, int k)
 {
     Knnresult* result = new Knnresult;
-    result->nidx = new int*[m];
-    for (int i = 0; i < m; i++)
-        result->nidx[i] = new int[k];
-
-    result->ndist = new double*[m];
-    for (int i = 0; i < m; i++)
-        result->ndist[i] = new double[k];
-    
+    result->nidx = new int[m * k];
+    result->ndist = new double[m * k];
     result->m = m;
     result->k = k;
 
